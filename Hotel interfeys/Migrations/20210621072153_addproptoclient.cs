925dbd3d60c603d33bb560b1c1042302a82568da
ShohruhUzDev <shohruh7464784@mail.ru>
2021-06-21 16:59:43 +0500

    add new migration

---
+++ Hotel interfeys/Migrations/20210621072153_addproptoclient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel_interfeys.Migrations
+{
+    public partial class addproptoclient : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<long>(
+                name: "Cost",
+                table: "Rooms",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfArrival",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfDepartur",
+                table: "Clients",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Cost",
+                table: "Rooms");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfArrival",
+                table: "Clients");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfDepartur",
+                table: "Clients");
+        }
+    }
+}
