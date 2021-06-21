1510ee4732a37e3e5fbc6d2412f3efaebfba3c0c
ShohruhUzDev <shohruh7464784@mail.ru>
2021-06-21 17:38:16 +0500

    add new comments

--- Hotel interfeys/MainWindow.xaml.cs
+++ Hotel interfeys/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Itenso.TimePeriod;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +23,56 @@ namespace Hotel_interfeys
     /// </summary>
     public partial class MainWindow : Window
     {
+        DataTable dt = new DataTable();
         public MainWindow()
         {
             InitializeComponent();
+            dt.Columns.Add("Iyun");
+            dt.Columns.Add("Iyul");
+            dt.Columns.Add("Avgust");
+            dt.Columns.Add("Sentabr");
+            dt.Columns.Add("Oktabr");
+            dt.Columns.Add("Noyabr");
+            dt.Columns.Add("Dekabr");
+            dt.Columns.Add("Yanvar");
+            dt.Columns.Add("Fevral");
         }
+         
+            private void Window_Loaded(object sender, RoutedEventArgs e)
+             {
+           
+
+            }
+
+        private void maximize_btn_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.WindowState == WindowState.Normal)
+            {
+
+                this.WindowState = WindowState.Maximized;
+                maximize_icon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowRestore;
+            }
+            else
+
+            {
+                maximize_icon.Kind = MaterialDesignThemes.Wpf.PackIconKind.WindowMaximize;
+                this.WindowState = WindowState.Normal;
+            }
+
+        }
+
+        private void minimize_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+
+        }
+
+        private void close_btn_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        // FillKalendar();
+
     }
 }
