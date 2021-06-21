bd48715a49e37351a3ee21a3533b8d032657ef88
ShohruhUzDev <shohruh7464784@mail.ru>
2021-06-21 12:10:24 +0500

    add HotelContext

---
+++ Hotel interfeys/Models/HotelContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_interfeys.Models
+{
+    class HotelContext:DbContext
+    {
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Room> Rooms { get; set; }
+
+        public HotelContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb; Database=Hoteldb; Trusted_Connection=true;");
+        }
+
+    }
+}
