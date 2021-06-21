88e7a00c7578532f2237051353b683eeceacd3c7
ShohruhUzDev <shohruh7464784@mail.ru>
2021-06-21 17:00:23 +0500

    add employee class

---
+++ Hotel interfeys/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_interfeys.Models
+{
+   // [TableName("employee")]
+   public  class Employee
+    {
+        public int Id { get; set; }
+        public string  FirstName { get; set; }
+        public string  LastName { get; set; }
+        public string  Address { get; set; }
+        public string  Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string  Email { get; set; }
+    }
+}
